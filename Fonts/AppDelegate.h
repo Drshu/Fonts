598a50d9bdfd5b96fa725bb83829443ac8c6201f
Shucheng <741932183@qq.com>
2016-03-01 14:22:23 +0800

    Initial Commit

---
+++ Fonts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fonts
+//
+//  Created by shucheng on 16/3/1.
+//  Copyright © 2016年 shucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
